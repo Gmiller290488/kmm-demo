36236a674500391f79798fbc1f47e90b3bdea79e
Gareth Miller <gmiller290488@gmail.com>
2020-11-02 17:04:53 +0000

    Initial commit

---
+++ shared/src/androidTest/kotlin/com/example/kmmdemo/shared/androidTest.kt
@@ -0,0 +1,12 @@
+package com.example.kmmdemo.shared
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class AndroidGreetingTest {
+
+    @Test
+    fun testExample() {
+        assertTrue("Check Android is mentioned", Greeting().greeting().contains("Android"))
+    }
+}
